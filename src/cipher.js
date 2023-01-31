a6aa05b1727973328c0a275f2040efe65eaa589b
Edimara Arcanjo <edimaraarcanjo21@gmail.com>
2023-01-31 15:11:56 -0300

    botões-respondendo

--- src/cipher.js
+++ src/cipher.js
@@ -3,3 +3,14 @@ const cipher = {
 };
 
 export default cipher;
+
+// ((codigoDaletraASC - cod1aLetra + desloc) % tamDoAlfabeto) + cod1aLetra
+// String.fromCharCode()
+
+function codificar (deslocamentoC.Number.value, mensagemC) {
+  formula
+  return
+
+}
+
+chamar a função?
--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+let deslocamentoC = document.getElementById('textoDeslocC')
+let mensagemC = document.getElementById('msgC')
+let botaoC = document.getElementById('botaoC')
+let cifrado = document.getElementById('cifrado')
+
+let txtDeslocD = document.getElementById('textoDeslocD')
+let mensagemD = document.getElementById('msgD')
+let botaoD = document.getElementById('botaoD')
+let decifrado = document.getElementById('decifrado')
+
+function encode(e) {
+  e.preventDefault()
+  cifrado.innerHTML = mensagemC.value
+}
+
+
+botaoC.addEventListener('click', encode) 
+
+function decode(d) {
+  d.preventDefault()
+  decifrado.innerHTML = mensagemD.value
+}
+
+botaoD.addEventListener('click', decode)
